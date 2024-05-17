b23177e6ed1745ba43202daa2dc47a119b76705c
Jose Wu <josewu@uictech.com.tw>
2024-05-17 13:36:44 +0800

    feat: 新增貓咪圖片 api

---
+++ FileProviderTest/Controllers/CatImageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProviderTest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CatImageController : ControllerBase
+    {
+        private readonly IFileProvider _fileProvider;
+        public CatImageController(IFileProvider fileProvider) => _fileProvider = fileProvider;
+        
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetCat([FromRoute] string id) 
+        {
+            try
+            {
+                var fileInfo = _fileProvider.GetFileInfo($"cat{id}.png");
+                return File(fileInfo.CreateReadStream(), "image/png");
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
--- FileProviderTest/Program.cs
+++ FileProviderTest/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// fileprovider
+//builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory() + "/wwwroot"));
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory() + "/catimages/"));
 
 var app = builder.Build();
 
